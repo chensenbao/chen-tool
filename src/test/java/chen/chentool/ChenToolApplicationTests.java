854a6d8254657e230d4b6617a0447b82eede2452
chensenbao <chensenbao123@163.com>
2024-05-10 15:45:19 +0800

    1

---
+++ src/test/java/chen/chentool/ChenToolApplicationTests.java
@@ -0,0 +1,13 @@
+package chen.chentool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChenToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
