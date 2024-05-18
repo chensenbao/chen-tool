46f0e8c60ab0106f9f30563a16f0b5ae3bcd53b5
chensenbao <chensenbao123@163.com>
2024-05-18 10:30:29 +0800

    定时器配置

---
+++ src/main/java/chen/chentool/sort/MapSort.java
@@ -0,0 +1,19 @@
+package chen.chentool.sort;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * @description: map排序
+ * 记一种map歪门邪道的排序方式
+ * @author: sen
+ */
+@Component
+public class MapSort {
+
+    public String linkMapSort(Map<String, Object> param){
+
+        return null;
+    }
+}
---
+++ src/test/java/chen/chentool/sort/MapSortTest.java
@@ -0,0 +1,11 @@
+package chen.chentool.sort;
+
+import static org.junit.Assert.*;
+
+/**
+ * @description:
+ * @author: sen
+ */
+public class MapSortTest {
+
+}
