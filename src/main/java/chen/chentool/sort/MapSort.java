51a81b0785d650ba4899892aed81111c978b629b
chensenbao <chensenbao123@163.com>
2024-05-24 17:04:54 +0800

    对Map<String, Map<String, Object>>对象的某个字段进行map排，返回有序的map

--- src/main/java/chen/chentool/sort/MapSort.java
+++ src/main/java/chen/chentool/sort/MapSort.java
@@ -1,8 +1,10 @@
 package chen.chentool.sort;
 
+import cn.hutool.core.convert.Convert;
+import org.springframework.cglib.core.Converter;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
+import java.util.*;
 
 /**
  * @description: map排序
@@ -12,8 +14,29 @@ import java.util.Map;
 @Component
 public class MapSort {
 
-    public String linkMapSort(Map<String, Object> param){
-
-        return null;
+    /**
+     *  利用LinkedHashMap的插入有序性进行map重排序
+     */
+    public Map<String,Object> linkMapSort(Map<String, Map<String, Object>> param, String sortKey) {
+//        把原始map对象转成list对象
+        List<Map<String, Object>> list = new ArrayList<>();
+        for (Map.Entry<String, Map<String, Object>> entry : param.entrySet()) {
+            String key = entry.getKey();
+            Map<String, Object> value = entry.getValue();
+//            添加map对象的key
+            value.put("key", key);
+            list.add(value);
+        }
+//        保持插入顺序的map有序对象
+        LinkedHashMap<String, Object> linkedHashMap = new LinkedHashMap<>();
+//        对list的目标字段进行排序，遍历写入linkedHashMap
+        list.stream().sorted(Comparator.comparing(i -> Convert.toInt(i.get(sortKey), 0)
+                , Comparator.reverseOrder()))
+                .forEach(i->{
+                    String key = Convert.toStr(i.get("key"));
+                    i.remove("key");
+                    linkedHashMap.put(key, i);
+                });
+        return linkedHashMap;
     }
 }
--- src/test/java/chen/chentool/sort/MapSortTest.java
+++ src/test/java/chen/chentool/sort/MapSortTest.java
@@ -1,11 +1,48 @@
 package chen.chentool.sort;
 
+import cn.hutool.core.convert.Convert;
+import cn.hutool.core.util.RandomUtil;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 /**
  * @description:
  * @author: sen
  */
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
 public class MapSortTest {
+    @Autowired
+    private MapSort mapSort;
 
+    @Test
+    public void linkMapSort() {
+        Map<String, Map<String,Object>> param = new HashMap<>();
+        for (int i = 0; i < 10; i++) {
+            param.put("map"+i,getMap());
+        }
+        Map<String, Object> destMap = mapSort.linkMapSort(param, "number");
+        System.out.println(destMap.toString());
+    }
+
+    /**
+     * @return 随机map对象
+     */
+    private Map<String,Object> getMap(){
+        Map<String,Object> map = new HashMap<>();
+        String name = RandomUtil.randomString(5);
+        int number = RandomUtil.randomInt(1000, 9999);
+        map.put("name",name);
+        map.put(name,name);
+        map.put("number", number);
+        return map;
+    }
 }
