25686926cd87f84e7d8e303874fe41c5001266b2
chensenbao <chensenbao123@163.com>
2024-06-07 14:47:42 +0800

    解析直达车次信息

--- src/main/java/chen/chentool/trainTicket/StationService.java
+++ src/main/java/chen/chentool/trainTicket/StationService.java
@@ -4,13 +4,18 @@ import chen.chentool.trainTicket.dao.StationDao;
 import chen.chentool.trainTicket.entity.StationEntity;
 import chen.chentool.trainTicket.entity.TicketDTO;
 import chen.chentool.trainTicket.entity.TicketReponse;
+import chen.chentool.trainTicket.entity.TrainBO;
 import chen.chentool.util.GsonUtil;
 import chen.chentool.util.HttpsUtil;
+import cn.hutool.core.collection.ListUtil;
+import cn.hutool.core.convert.Convert;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.HttpUtil;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -81,12 +86,82 @@ public class StationService {
         TicketReponse.Data data = ticketReponse.getData();
         //车站字典
         Map<String, Object> map = data.getMap();
-        //车次信息
+        //车次信息列表
         List<String> result = data.getResult();
+        if (result==null||result.isEmpty()){
+            return;
+        }
+        List<TrainBO> trainList=this.getParseTrainInfo(result,map);
 
 
 
         System.out.println(response);
+    }
+
+    /**
+     * 解析车次信息
+     * @param trainList 解析前车次列表
+     * @param stationMap 车站字典列表
+     * @return 解析后的车次列表
+     */
+    private List<TrainBO> getParseTrainInfo(List<String> trainList, Map<String, Object> stationMap) {
+        List<TrainBO> result=new ArrayList<>();
+        for (String str : trainList) {
+            String[] train = StrUtil.split(str, "|");
+            TrainBO trainBO = new TrainBO();
+            trainBO.setSecretStr(train[0]);
+            trainBO.setButtonTextInfo(train[1]);
+            trainBO.setTrainNo(train[2]);
+            trainBO.setStationTrainCode(train[3]);
+            trainBO.setStartStationTelecode(train[4]);
+            trainBO.setEndStationTelecode(train[5]);
+            trainBO.setFromStationTelecode(train[6]);
+            trainBO.setToStationTelecode(train[7]);
+            trainBO.setStartTime(train[8]);
+            trainBO.setArriveTime(train[9]);
+            trainBO.setLishi(train[10]);
+            trainBO.setCanWebBuy(train[11]);
+            trainBO.setYpInfo(train[12]);
+            trainBO.setStartTrainDate(train[13]);
+            trainBO.setTrainSeatFeature(train[14]);
+            trainBO.setLocationCode(train[15]);
+            trainBO.setFromStationNo(train[16]);
+            trainBO.setToStationNo(train[17]);
+            trainBO.setIsSupportCard(train[18]);
+            trainBO.setControlledTrainFlag(train[19]);
+            trainBO.setGgNum("".equals(train[20])?"--":train[20]);
+            trainBO.setGrNum("".equals(train[21])?"--":train[21]);
+            trainBO.setQtNum("".equals(train[22])?"--":train[22]);
+            trainBO.setRwNum("".equals(train[23])?"--":train[23]);
+            trainBO.setRzNum("".equals(train[24])?"--":train[24]);
+            trainBO.setTzNum("".equals(train[25])?"--":train[25]);
+            trainBO.setWzNum("".equals(train[26])?"--":train[26]);
+            trainBO.setYbNum("".equals(train[27])?"--":train[27]);
+            trainBO.setYwNum("".equals(train[28])?"--":train[28]);
+            trainBO.setYzNum("".equals(train[29])?"--":train[29]);
+            trainBO.setZeNum("".equals(train[30])?"--":train[30]);
+            trainBO.setZyNum("".equals(train[31])?"--":train[31]);
+            trainBO.setSwzNum("".equals(train[32])?"--":train[32]);
+            trainBO.setSrrbNum("".equals(train[33])?"--":train[33]);
+            trainBO.setYpEx(train[34]);
+            trainBO.setSeatTypes(train[35]);
+            trainBO.setExchangeTrainFlag(train[36]);
+            trainBO.setHoubuTrainFlag(train[37]);
+            trainBO.setHoubuSeatLimit(train[38]);
+            trainBO.setYpInfoNew(train[39]);
+            trainBO.setDwFlag(train[46]);
+            trainBO.setStopcheckTime(train[48]);
+            trainBO.setCountryFlag(train[49]);
+            trainBO.setLocalArriveTime(train[50]);
+            trainBO.setLocalStartTime(train[51]);
+            trainBO.setBedLevelInfo(train[53]);
+            trainBO.setSeatDiscountInfo(train[54]);
+            trainBO.setSaleTime(train[55]);
+            trainBO.setFromStationName(Convert.toStr(stationMap.get(train[6])));
+            trainBO.setToStationName(Convert.toStr(stationMap.get(train[7])));
+            result.add(trainBO);
+        }
+        return result;
     }
 
     private String getTicketUrl(TicketDTO ticketDTO) {
--- src/main/java/chen/chentool/trainTicket/entity/TrainBO.java
+++ src/main/java/chen/chentool/trainTicket/entity/TrainBO.java
@@ -8,5 +8,90 @@ import lombok.Data;
  */
 @Data
 public class TrainBO {
-
+    private String secretStr;
+    private String buttonTextInfo;
+    /**
+     * 车次编码，用于查询车次信息
+     */
+    private String trainNo;
+    /**
+     * 车次
+     */
+    private String stationTrainCode;
+    private String startStationTelecode;
+    private String endStationTelecode;
+    private String fromStationTelecode;
+    private String toStationTelecode;
+    /**
+     * 出发时间
+     * HH:mm
+     */
+    private String startTime;
+    /**
+     * 到达时间
+     * HH:mm
+     */
+    private String arriveTime;
+    /**
+     * 历时
+     * HH:mm
+     */
+    private String lishi;
+    private String canWebBuy;
+    private String ypInfo;
+    /**
+     * 出发日期
+     */
+    private String startTrainDate;
+    private String trainSeatFeature;
+    private String locationCode;
+    private String fromStationNo;
+    private String toStationNo;
+    private String isSupportCard;
+    private String controlledTrainFlag;
+    private String ggNum;
+    private String grNum;
+    private String qtNum;
+    private String rwNum;
+    private String rzNum;
+    private String tzNum;
+    /**
+     * 无座
+     */
+    private String wzNum;
+    private String ybNum;
+    private String ywNum;
+    /**
+     * 一等座
+     */
+    private String yzNum;
+    private String zyNum;
+    /**
+     * 二等座
+     */
+    private String zeNum;
+    private String swzNum;
+    private String srrbNum;
+    private String ypEx;
+    private String seatTypes;
+    private String exchangeTrainFlag;
+    private String houbuTrainFlag;
+    private String houbuSeatLimit;
+    private String ypInfoNew;
+    private String dwFlag;
+    private String stopcheckTime;
+    private String countryFlag;
+    private String localArriveTime;
+    private String localStartTime;
+    private String bedLevelInfo;
+    private String seatDiscountInfo;
+    private String saleTime;
+    /**
+     * 出发站
+     */
+    private String fromStationName;
+    /**
+     * 到达站
+     */
+    private String toStationName;
 }
